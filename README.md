ab819b630f4cd4b1def14a0a0e85ee4004272471
ubaumann <github@m.ubaumann.ch>
2023-08-14 23:16:25 +0200

    Add client tests

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 
 The lab is heavily based on the 5-stage Clos fabric from ContainerLab (https://containerlab.dev/lab-examples/min-5clos/).
 
-The client1 is replaced with a Python container image and NUTS will be installed on it. 
+The client1 is replaced with a Python container image and NUTS will be installed on it. Clients 2 to 4 are running a Linux container with an SSH service. 
 
 The router images are Nokia SR Linux. To be able to use NAPALM, the community driver (https://github.com/napalm-automation-community/napalm-srlinux) is used.
 
--- configs/client1.sh
+++ configs/client1.sh
@@ -9,9 +9,13 @@ ip addr add 1000:10:0:0::25/127 dev eth1
 echo 0 > /proc/sys/net/ipv6/conf/eth1/accept_ra
 
 pip install --update pip
-# pip install nuts
+pip install nuts
 # pip install git+https://github.com/network-unit-testing-system/nuts.git@branch_name
-pip install git+https://github.com/network-unit-testing-system/nuts.git
+# pip install git+https://github.com/network-unit-testing-system/nuts.git
 
 
 pip install git+https://github.com/napalm-automation-community/napalm-srlinux
+
+
+# Client tests are still in a early alpha phase
+pip install git+https://github.com/network-unit-testing-system/nuts-clinet-tests.git
